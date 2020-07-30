d4ff5345948757a3b7d6928c3a9284de09ac9b6d
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-30 21:22:15 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,29 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+   let hour=parseInt(time);
+    let morningTime = hour<12;
+    let afternoonTime=hour>=12 && hour<=17;
+    let eveningTime=hour>17 && hour<=24;
+  switch(true) {
+
+    case morningTime :
+    return "Good Morning";
+          break;
+      case afternoonTime:
+        return "Good Afternoon";
+            break;
+        case eveningTime:
+        return "Good Evening"
+              break;
+          default:
+          return "wrong, it should be a number less than 25";
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  const greeting =document.querySelector('#greeting');
+  greeting.innerText=message;
+}
